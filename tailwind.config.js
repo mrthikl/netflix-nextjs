2e445fdf3085f5c3f0b5d023c8ed15bab6457270
Thi Nguyen <nguyenthisc1@gmai.com>
2022-09-17 19:11:40 +0700

    build

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  important: true,
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      container: {
+        centered: true,
+      },
+      textShadow: {
+        md: '2px 2px 4px rgb(0 0 0 / 45%);',
+      },
+      backgroundImage: {
+        'gradient-to-b':
+          'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);',
+      },
+    },
+  },
+  plugins: [require('tailwindcss-textshadow'), require('tailwind-scrollbar-hide'), require('tailwind-scrollbar')],
+}
